baa251f586cfa07bf41fc6f6ab37c916b5a62dc8
Anelisiwe <anelisiwe063@gmail.com>
2021-05-25 17:15:04 +0200

    before demo

--- functions/addRegistrations.js
+++ functions/addRegistrations.js
@@ -1,26 +1,41 @@
-function addRegistrations(){
+function addRegistrations(existingReg){
 
-    //var reg_No; 
-    var letters = /^[A-Za-z]+$/;
+    var arrReg =  existingReg || [];
+    var letters =  /[A-Z]+\s\d+/gi
+
+
 
     function enterRegistrationsNo(regNo){
-         
         var numberPlate = regNo.trim()
-        if (numberPlate.match(letters)){
+         if (numberPlate.match(letters)){
         return numberPlate;
+        }
 
-    }
-
-    else {
+       else {
 
         return("invalid registration");
-    }     
+        }     
   
     }
 
-      
+    function addRegOnArr(regArr){
+
+        var regNum01 = regArr;
+        if(!arrReg.includes(regNum01)){
+            arrReg.push(regNum01);
+        }
+    }
+
+    function getRegistrations(){
+        return arrReg
+    }
+
+  
+
 
     return{
-        enterRegistrationsNo
+        enterRegistrationsNo,
+        addRegOnArr,
+        getRegistrations
      } 
 }
--- js/registrationNoPlate.js
+++ js/registrationNoPlate.js
@@ -1,12 +1,130 @@
-var regElement = document.querySelector('.regNum') //refence
+var regElem = document.querySelector('.regNum') //refence
 var addButtonElement = document.querySelector('.add')
-var showAddedRegElement = document.querySelector('.recentReg')
+var showAllButtonElement = document.querySelector('.show-all')
+//var showAddedRegElement = document.querySelector('.recentReg')
+var showAllRegistrationsElement = document.getElementById('countReg')
+var resetButtonElem = document.querySelector('.reset')
+// var checkedRadioElement = document.querySelector("input[name='location']:checked")
+var showElementButton = document.querySelector('.show')
+var showRegForSelected = document.getElementById('showSelected')
 
-var regInstance = addRegistrations();
+
+if(localStorage['reg']){
+    var storeRegistrationNimbers = JSON.parse(localStorage.getItem('reg'));
+}
+
+var regInstance = addRegistrations(storeRegistrationNimbers);
+//let counter =regInstance.addRegOnArr()
+
+var unorderd = document.getElementById('list');
+    
 function addReg(){
+
+
+
+  //if(regElem.value)
    
-    regElement = regInstance.enterRegistrationsNo(regElement.value);
-    showAddedRegElement.innerHTML= addRegistrationNumberInstance.enterRegistrationsNo(regElement.value);
+  var validReg =  regInstance.enterRegistrationsNo(regElem.value);
+   // showAddedRegElement.innerHTML= regElem.value;
+     let element = document.createElement("li")
+     var reglist = document.createTextNode(validReg)
+     element.appendChild(reglist)
+     unorderd.appendChild(element)
+    // console.log(unorderd);
+    // document.regNum.appendChild(ul)
+
+    regInstance.addRegOnArr(regElem.value)
+   localStorage.setItem("reg", JSON.stringify(regInstance.getRegistrations()));
+    
+
+   regElem.value = '';
+  
+}
+
+   function showSelectedRegTown(){
+
+    var checkedRadioBtn = document.querySelector("input[name='location']:checked");
+       //var counter=0;
+       var arrReg= regInstance.getRegistrations();
+       var selectedregTown = [];
+      //  console.log(arrReg)
+     
+        if(checkedRadioBtn.value === "bellville") {
+            for(var i =0; i<=arrReg.length;i++) {
+              if(arrReg[i].startsWith('CY')){
+                // console.log(regNew[i]);
+                let bellivelleReg = document.createElement("li")
+                var bellReg= document.createTextNode(arrReg[i])
+                 bellivelleReg.appendChild(bellReg)
+                 showRegForSelected.appendChild(bellivelleReg)
+                 checkedRadioBtn.checked = false;
+
+
+                  //selectedregTown.push(regNew[i])
+                 // showRegForSelected.innerHTML = selectedregTown;
+
+              }
+              // console.log(arrReg) ;
+            }      
+        }
+
+       else if(checkedRadioBtn.value === "capeTown") {
+            for(var i =0; i<=arrReg.length;i++) {
+              if(arrReg[i].startsWith('CA')){
+                let capeReg = document.createElement("li")
+                var capetownReg= document.createTextNode(arrReg[i])
+                 capeReg.appendChild(capetownReg)
+                 showRegForSelected.appendChild(capeReg) 
+                 checkedRadioBtn.checked = false;             
+              }
+              // console.log(arrReg) ;
+            }
+       }
+
+       else if(checkedRadioBtn.value === "paarl") {
+        for(var i =0; i<=arrReg.length;i++) {
+          if(arrReg[i].startsWith('CJ')){
+            let paarlReg = document.createElement("li")
+                var paar= document.createTextNode(arrReg[i])
+                 paarlReg.appendChild(paar)
+                 showRegForSelected.appendChild(paarlReg)  
+                 checkedRadioBtn.checked = false;
+          }
+          // console.log(arrReg) ;
+          //showRegForSelected.innerHTML = arrReg;
+        }
    }
 
+   
+      
+   else {
+       "There's no registration"
+      }
+      
+      
+   }
+
+   function showAll(){
+    var showAllTownArrs = regInstance.getRegistrations()
+   for(var i=0; i<=showAllTownArrs.length; i++){
+   console.log(showAllTownArrs[i]);
+   let allArrShow = document.createElement("li")
+   var allTownArrs = document.createTextNode(showAllTownArrs[i])
+   allArrShow.appendChild(allTownArrs)
+   showAllRegistrationsElement.appendChild(allArrShow);
+   }
+ }
+
+ function resetStorage(){
+  //localStorage.clear()
+  regInstance.getRegistrations().clear()
+  //countRegistrationElement.clear()
+ // Location.reload()
+
+}
+  
+ 
    addButtonElement.addEventListener('click', addReg)
+   showAllButtonElement.addEventListener('click', showAll)
+   resetButtonElem.addEventListener('click',resetStorage )
+   showElementButton.addEventListener('click',showSelectedRegTown )
